a5c8dcdc724073a16b8fc0ea259aa27e19fade90
renyongzheng <zheng.ren@northeastern.edu>
2023-01-23 17:51:49 -0800

    finished

---
+++ Lab1VectorMotion/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 7)
+        {
+            Console.WriteLine("Invalid number of arguments. Correct usage: dotnet run startX startY startYZ finalX finalY finalZ time");
+            return;
+        }
+
+        int[] inputs = args.Select(int.Parse).ToArray();
+        int startX = inputs[0];
+        int startY = inputs[1];
+        int startZ = inputs[2]; 
+        int finalX = inputs[3];
+        int finalY = inputs[4];
+        int finalZ = inputs[5];
+        int time = inputs[6];
+        // int newX = startX + finalX;
+        // int newY = startY + finalY;
+        // int newZ = startZ + finalZ;
+
+        double distance = Math.Sqrt(Math.Pow(finalX - startX, 2) + Math.Pow(finalY - startY, 2) + Math.Pow(finalZ - startZ, 2));
+        double velocity = distance / time;
+
+        
+        Console.WriteLine("start position: " + startX + " " + startY + " " + startZ);
+        Console.WriteLine("final position: " + finalX + " " + finalY + " " + finalZ);
+        Console.WriteLine("time: " + time);
+
+        Console.WriteLine("distance: " + distance);
+        Console.WriteLine("Speed: " + velocity);
+
+        double trackX = (double)(finalX - startX)/10;
+        double trackY = (double)(finalY - startY)/10;
+        double trackZ = (double)(finalZ - startZ)/10;
+
+        double t = 0.0;
+        double[] currentPosition = new double[] { startX, startY, startZ };
+        double distanceTraveled = 0.0;
+        double timeIncrement = (double)time/10;
+        int iteration = 0;
+
+        for (iteration = 1; iteration <= 10; iteration++) {
+            // Calculate distance traveled
+            distanceTraveled = Math.Sqrt(Math.Pow(trackX * iteration, 2) + Math.Pow(trackY * iteration, 2) + Math.Pow(trackZ * iteration, 2));
+
+            // Calculate current position
+            currentPosition[0] = currentPosition[0] + trackX;
+            currentPosition[1] = currentPosition[1] + trackY;
+            currentPosition[2] = currentPosition[2] + trackZ;
+
+            // Calculate time
+            t += timeIncrement;
+
+            // Print iteration information
+            Console.WriteLine("iteration {0}:", iteration);
+            Console.WriteLine("distance traveled {0}", Math.Round(distanceTraveled, 2));
+            Console.WriteLine("current position: [{0}, {1}, {2}]", Math.Round(currentPosition[0],2), Math.Round(currentPosition[1],2), Math.Round(currentPosition[2],2));
+            Console.WriteLine("time: {0}", t);
+        }
+        
+    }
+
+        
+}
+
---
+++ sample-code-c/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // create a list of integers
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        // use a foreach loop to iterate through the list and print each number
+        Console.Write("Numbers: ");
+        foreach (int num in numbers)
+        {
+            Console.Write(num + " ");
+        }
+
+        Console.WriteLine();
+
+        // use a for loop to calculate the factorial of each number in the list
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            int factorial = 1;
+            for (int j = 1; j <= numbers[i]; j++)
+            {
+                factorial *= j;
+            }
+            numbers[i] = factorial;
+        }
+        foreach (int num in numbers)
+        {
+            Console.Write(num + " ");
+        }
+
+        // use a lambda expression to print the list of factorials
+        // numbers.ForEach(n => Console.Write(n + " "));
+    }
+}
